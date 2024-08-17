c28afc41e858642912fb64a5c43116bc1b2d4558
werent4 <dimavod03@gmail.com>
2024-08-17 12:14:06 +0000

    run updated

--- run_training.sh
+++ run_training.sh
@@ -4,7 +4,7 @@ python src/run.py --tokenizer 'google/mt5-small' \
     --dataset 'ALL' \
     --max_length 128 \
     --batch_size 15 \
-    --learning_rate 1e-7 \
+    --learning_rate 1e-5 \
     --weight_decay 3e-3 \
     --num_epochs 1 \
     --start_from_batch 0 \
