bcba87b0523f86ba958cb1a8274c74ca1bf8b1b1
werent4 <dimavod03@gmail.com>
2024-08-17 13:25:23 +0000

    save updated

--- run_training.sh
+++ run_training.sh
@@ -8,4 +8,4 @@ python src/run.py --tokenizer 'google/mt5-small' \
     --weight_decay 3e-3 \
     --num_epochs 1 \
     --start_from_batch 0 \
-    --save_each_steps 100000
+    --save_each_steps 20000
